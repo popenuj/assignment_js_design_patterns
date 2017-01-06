9606d14b868fe1b647120b438f507fd643805a47
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-06 11:00:49 -0800

    added columns and card backs starting flipping

--- card_matcher.js
+++ card_matcher.js
@@ -1,41 +1,95 @@
 
 var cardMatcher = {};
 
-cardMatcher.cardMin = 4;
-cardMatcher.cardMax = 60;
+cardMatcher.columnMin = 2;
+cardMatcher.columnMax = 6;
+cardMatcher.cards = []
 
 cardMatcher.model = {
+  columnCount: 0,
   cardCount: 0,
+  cards: cardMatcher.cards,
 
-  setCardCount: function(cardCountInput) {
-    var inputInt = parseInt(cardCountInput);
-    if (inputInt % 2 !== 0 || inputInt < cardMatcher.cardMin || inputInt > cardMatcher.cardMax) {
-      alert("Please enter an even number between 4 and 60.")
+  setColumnCount: function(columnCountInput) {
+    var inputInt = parseInt(columnCountInput);
+    if (inputInt % 2 !== 0 || inputInt < cardMatcher.columnMin || inputInt > cardMatcher.columnMax) {
+      alert("Please enter an even number between " + String(cardMatcher.columnMin) + " and " + String(cardMatcher.columnMax) + ".");
     } else {
-      this.cardCount = inputInt;
+      this.columnCount = inputInt;
+      this.cardCount = Math.pow(inputInt, 2);
+    };
+  },
+
+  getColumnClass: function() {
+    return "thumbnail col-xs-" + String(12 / this.columnCount);
+  },
+
+  createColumn: function() {
+    var image = '<img src="card-back.jpg">';
+    var $div = $("<div>").addClass(this.getColumnClass()).html(image);
+    return $div;
+  },
+
+  generateCards: function() {
+    var i = this.cardCount / 2;
+    while (i--) {
+      this.cards.push(String(i));
+      this.cards.push(String(i));
+    }
+    this.shuffleCards();
+  },
+
+  shuffleCards: function() {
+    for (var i = this.cards.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [this.cards[i - 1], this.cards[j]] = [this.cards[j], this.cards[i - 1]];
     }
   }
-}
+
+};
 
 cardMatcher.view = {
   init: function() {
   },
 
-  getCardCount: function() {
-    return window.prompt("Type in an even number of cards to play with, between " + String(cardMatcher.cardMin) + " and " + String(cardMatcher.cardMax) + ".", String(cardMatcher.cardMin));
+  getColumnCount: function() {
+    return window.prompt("Type in an even number of columns to play with, between " + cardMatcher.columnMin + " and " + cardMatcher.columnMax + ".", cardMatcher.columnMin);
+  },
+
+  setCardDivs: function(column, cardCount) {
+    var $containingRow = $(".row");
+    var i = cardCount
+    while (i--) {
+      $containingRow.append(column.clone());
+    };
+  },
+
+  setCardIds: function() {
+
   }
-}
+
+};
 
 cardMatcher.controller = {
   init: function() {
-    this.checkCardCount();
+    this.checkColumnCount();
+    this.getAndSetColumns();
+    cardMatcher.model.generateCards();
   },
 
-  checkCardCount: function() {
-    while (!cardMatcher.model.cardCount) {
-      cardMatcher.model.setCardCount(cardMatcher.view.getCardCount());
+  checkColumnCount: function() {
+    while (!cardMatcher.model.columnCount) {
+      cardMatcher.model.setColumnCount(cardMatcher.view.getColumnCount());
     }
-  }
-}
+  },
 
-cardMatcher.controller.init();
+  getAndSetColumns: function() {
+    var column = cardMatcher.model.createColumn();
+    var cardCount = cardMatcher.model.cardCount
+    cardMatcher.view.setCardDivs(column, cardCount);
+  }
+};
+
+$(document).ready(function() {
+  cardMatcher.controller.init();
+});
